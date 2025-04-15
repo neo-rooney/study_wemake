383a745e070deddf058649a31f69688d7a1e6aed
rooney <bch3454@gmail.com>
2025-04-16 00:12:41 +0900

    feat: Navigation 추가

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,8 +1,7 @@
-
 export default function HomePage() {
-    return (
-        <div className="container mx-auto px-4 py-8">
-            <h1 className="text-4xl font-bold mb-4">홈 페이지</h1>
-        </div>
-    );
-} 
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-4xl font-bold mb-4">홈 페이지</h1>
+    </div>
+  );
+}
