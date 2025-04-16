82739ad5a7e7ba65c05dbe34169413187ee042d1
rooney <bch3454@gmail.com>
2025-04-16 16:46:06 +0900

    feat: Product 카드 제작

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,7 +1,28 @@
+import { ProductCard } from "~/features/products/components/product-card";
+
 export default function HomePage() {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-4xl font-bold mb-4">홈 페이지</h1>
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our community today.
+          </p>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <ProductCard
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- app/root.tsx
+++ app/root.tsx
@@ -44,7 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <div>
+    <div className="py-16">
       <Navigation
         isLoggedIn={true}
         hasMessages={true}
